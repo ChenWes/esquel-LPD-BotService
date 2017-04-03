4dc09ddfa4f292ea1c613390d1d57f69bc3620ce
Wes Chen /GET/IT <ChenWes@esquel.com>
2017-04-03 11:31:56 +0800

    change project file
    * delete no need function

--- messages/index.js
+++ messages/index.js
@@ -30,99 +30,19 @@ const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' +
 // Main dialog with LUIS
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] })
-.matches('SearchGarmentStyle',[
-    function(session,arg,next){
-        session.send('Welcome to the Hotels finder! We are analyzing your message: \'%s\'', session.message.text);
-        var garmentStyleEntity = builder.EntityRecognizer.findEntity(args.entities, 'GarmentStyleNo');
-        if (garmentStyleEntity) {
-            next({ response: garmentStyleEntity.entity });
-        } else {
-            builder.Prompts.text(session, 'Please enter garment style no');
-        }
-    },
-    function(session,results){
-        var garmentStyleNo = results.response;
-    
-        if(garmentStyleNo)
-        {
-            garmentStyleNo = garmentStyleNo.replace(/\s+/g, "");
-            searchHotels(garmentStyleNo)
-            .then(function (hotels) {
-                // args
-                session.send('I found %d hotels:', hotels.length);
-
-                var message = new builder.Message()
-                    .attachmentLayout(builder.AttachmentLayout.carousel)
-                    .attachments(hotels.map(hotelAsAttachment));
-
-                session.send(message);
-
-                // End
-                session.endDialog();
-            });
-        }else{
-            session.send('can not found garment style number from you message,please try again .');
-            session.endDialog();
-        }
-    }
-])
-.onDefault((session) => {
-    session.send('Sorry, I did not understand \'%s\'.', session.message.text);
-});
-
-
-//get image
-// Helpers
-function hotelAsAttachment(hotel) {
-    return new builder.HeroCard()
-        .title(hotel.name)
-        .subtitle('%d stars. %d reviews. From $%d per night.', hotel.rating, hotel.numberOfReviews, hotel.priceStarting)
-        .images([new builder.CardImage().url(hotel.image)])
-        .buttons([
-            new builder.CardAction()
-                .title('More details')
-                .type('openUrl')
-                .value('https://www.bing.com/search?q=hotels+in+' + encodeURIComponent(hotel.location))
-        ]);
-}
-
-
-
-var searchHotels= function (destination) {
-    return new Promise(function (resolve) {
-
-        // Filling the hotels results manually just for demo purposes
-        var hotels = [];
-        for (var i = 1; i <= 5; i++) {
-            hotels.push({
-                name: destination + ' Hotel ' + i,
-                location: destination,
-                rating: Math.ceil(Math.random() * 5),
-                numberOfReviews: Math.floor(Math.random() * 5000) + 1,
-                priceStarting: Math.floor(Math.random() * 450) + 80,
-                image: 'https://placeholdit.imgix.net/~text?txtsize=35&txt=Hotel+' + i + '&w=500&h=260'
-            });
-        }
-
-        hotels.sort(function (a, b) { return a.priceStarting - b.priceStarting; });
-
-        // complete promise with a timer to simulate async response
-        setTimeout(function () { resolve(hotels); }, 1000);
+    .onDefault((session) => {
+        session.send('sorry , i have no idea what you talking about.\"%s\"', session.message.text);
     });
-}
 
-
-
-
-bot.dialog('/', intents);    
+bot.dialog('/', intents);
 
 if (useEmulator) {
     var restify = require('restify');
     var server = restify.createServer();
-    server.listen(3978, function() {
+    server.listen(3978, function () {
         console.log('test bot endpont at http://localhost:3978/api/messages');
     });
-    server.post('/api/messages', connector.listen());    
+    server.post('/api/messages', connector.listen());
 } else {
     module.exports = { default: connector.listen() }
 }
