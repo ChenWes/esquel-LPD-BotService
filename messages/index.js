90f4ecd132c63b5879da60577045ca47f6d77596
Wes Chen /GET/IT <ChenWes@esquel.com>
2017-04-03 11:57:50 +0800

    change bot luis intents
    * add search garment style intents
    * add search fabirc intents
    * add search trim intents

--- messages/index.js
+++ messages/index.js
@@ -30,6 +30,85 @@ const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' +
 // Main dialog with LUIS
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+    .matches('SearchGarmentStyle', [
+        function (session, args, next) {
+            session.send('hi ,we are analyzing your message: \'%s\' for search garment style, please wait.', session.message.text);
+            logger.log('info', 'start Luis Intent SearchGarmentStyle from [%s]', session.message.text);
+            // try extracting entities
+            var garmentStyleEntity = builder.EntityRecognizer.findEntity(args.entities, 'GarmentStyleNo');
+            if (garmentStyleEntity) {
+                next({ response: garmentStyleEntity.entity });
+            } else {
+                // no entities detected, ask user for a garment style, same as get parameter from Luis
+                builder.Prompts.text(session, 'Please enter garment style no');
+            }
+        },
+        function (session, results) {
+            var garmentStyleNo = results.response;
+            if (garmentStyleNo) {
+                garmentStyleNo = garmentStyleNo.replace(/\s+/g, "");
+                // Async search WebAPI
+                session.send('found garment style number \'%s\' , search data from API', trimNo);
+                session.endDialog();
+            } else {
+                session.send('can not found garment style number from you message \'%s\' ', session.message.text);
+                session.endDialog();
+            }
+        }
+    ])
+    .matches('SearchFabric', [
+        function (session, args, next) {
+            session.send('hi ,we are analyzing your message: \'%s\' for search fabric, please wait.', session.message.text);
+            logger.log('info', 'start Luis Intent SearchFabric from [%s]', session.message.text);
+            // try extracting entities
+            var fabricEntity = builder.EntityRecognizer.findEntity(args.entities, 'FabricNo');
+            if (fabricEntity) {
+                next({ response: fabricEntity.entity });
+            } else {
+                // no entities detected, ask user for a fabric, same as get parameter from Luis
+                builder.Prompts.text(session, 'Please enter fabric no');
+            }
+        },
+        function (session, results) {
+            var fabricNo = results.response;
+            if (fabricNo) {
+                fabricNo = fabricNo.replace(/\s+/g, "");
+                // Async search WebAPI
+                session.send('found fabric number \'%s\' , search data from API', fabricNo);
+                session.endDialog();
+            } else {
+                session.send('can not found fabric number from you message \'%s\' ', session.message.text);
+                session.endDialog();
+            }
+        }
+    ])
+    .matches('SearchTrim', [
+        function (session, args, next) {
+            session.send('hi ,we are analyzing your message: \'%s\' for search trim, please wait.', session.message.text);
+            logger.log('info', 'start Luis Intent SearchTrim from [%s]', session.message.text);
+            // try extracting entities
+            var trimEntity = builder.EntityRecognizer.findEntity(args.entities, 'TrimNo');
+            if (trimEntity) {
+                next({ response: trimEntity.entity });
+            } else {
+                // no entities detected, ask user for a trim, same as get parameter from Luis
+                builder.Prompts.text(session, 'Please enter trim no');
+            }
+        },
+        function (session, results) {
+            var trimNo = results.response;
+            if (trimNo) {
+                trimNo = trimNo.replace(/\s+/g, "");
+                // Async search WebAPI
+                session.send('found trim number \'%s\' , search data from API', trimNo);
+                session.endDialog();
+            } else {
+                session.send('can not found trim number from you message \'%s\' ', session.message.text);
+                session.endDialog();
+            }
+        }
+    ])
+    .matches('Hello', builder.DialogAction.send('hi! welcome use Esquel LPD Bot, try asking me things like \'search germent style XXX\', \'search style XXX\' or \'style XXX\''))
     .onDefault((session) => {
         session.send('sorry , i have no idea what you talking about.\"%s\"', session.message.text);
     });
