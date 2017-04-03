350cb508e7fef2fecd72351a785c8856d328ab15
Wes Chen /GET/IT <ChenWes@esquel.com>
2017-04-03 13:37:20 +0800

    Revert "change setting for local debug"
    This reverts commit 0c5b9e06bacdef6c50ce4281848ec47b8baf2ecd.

--- messages/index.js
+++ messages/index.js
@@ -8,11 +8,11 @@ http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 
-var useEmulator = true;// (process.env.NODE_ENV == 'development');
+var useEmulator = (process.env.NODE_ENV == 'development');
 
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
-    appId: '79697cee-9391-48b7-b164-3036e3e9d105',//process.env['MicrosoftAppId'],
-    appPassword: 'JaHsadjeqzSddMrj5ZWuxCN',//process.env['MicrosoftAppPassword'],
+    appId: process.env['MicrosoftAppId'],
+    appPassword: process.env['MicrosoftAppPassword'],
     stateEndpoint: process.env['BotStateEndpoint'],
     openIdMetadata: process.env['BotOpenIdMetadata']
 });
@@ -20,8 +20,8 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
 var bot = new builder.UniversalBot(connector);
 
 // Make sure you add code to validate these fields
-var luisAppId = '3d117394-44c3-416f-ab11-96e8d451018d';//process.env.LuisAppId;
-var luisAPIKey = 'f21ea5607c6b43589c12e6099b0a9614'; //process.env.LuisAPIKey;
+var luisAppId = process.env.LuisAppId;
+var luisAPIKey = process.env.LuisAPIKey;
 var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
 
 const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
